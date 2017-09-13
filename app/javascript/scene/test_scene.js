29ad35b69979bef079b7eba77bfe8c2ec52f759b
シュール <shule517@gmail.com>
2017-09-14 01:18:46 +0900

    順番にアニメーションされるように修正

--- app/javascript/scene/test_scene.js
+++ app/javascript/scene/test_scene.js
@@ -1,12 +1,50 @@
 import { SutachooAnime } from 'anime';
 
 export default class TestScene extends enchant.Scene {
+  get input() {
+    return enchant.Core.instance.input;
+  }
+
   constructor() {
     super();
     let suta = new SutachooAnime();
     suta.x = 100;
     suta.y = 100;
     this.addChild(suta);
+
+    suta.walking();
+    suta.walking();
+    suta.right();
+    suta.left();
+    suta.oxox();
+    suta.walking();
+    suta.walking();
     suta.jakajan();
+    suta.walking();
+    suta.right();
+    suta.left();
+    suta.right();
+    suta.left();
+    suta.walking();
+    suta.walking();
+    suta.walking();
+
+    this.addEventListener('enterframe', () => {
+      if (this.input.left) {
+        suta.left();
+        suta.walking();
+      } else if (this.input.right) {
+        suta.right();
+        suta.walking();
+      } else if (this.input.up) {
+        suta.walking();
+      } else if (this.input.down) {
+        suta.walking();
+      } else if (this.input.a) {
+        suta.oxox();
+      } else if (this.input.b) {
+        suta.jakajan();
+      }
+    });
   }
 }
