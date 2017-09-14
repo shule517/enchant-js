c766e1101a85eb9f23e3804299eb70e100ce3ff1
シュール <shule517@gmail.com>
2017-09-14 20:03:28 +0900

    OpeningMapを視点が変更できるようにした

--- app/javascript/opening/scene.js
+++ app/javascript/opening/scene.js
@@ -11,14 +11,16 @@ export default class OpeningScene extends enchant.Scene {
     tengoku.scale = 0.8;
     this.addChild(tengoku);
 
-    for (let i = 0; i < 5; i++) {
-      for (let j = 0; j < 2; j++) {
-        let flower = new FlowerAnime();
-        flower.x = i * 580 - 50;
-        flower.y = 550 + j * 100;
-        this.addChild(flower);
-      }
-    }
+    // for (let i = 0; i < 5; i++) {
+    //   for (let j = 0; j < 2; j++) {
+    //     let flower = new FlowerAnime();
+    //     flower.x = i * 580 - 50;
+    //     flower.y = 550 + j * 100;
+    //     this.addChild(flower);
+    //   }
+    // }
+    let map = new Map();
+    this.addChild(map);
 
     let bigsight = new BigsightAnime();
     bigsight.x = 400;
@@ -26,5 +28,21 @@ export default class OpeningScene extends enchant.Scene {
     bigsight.scaleX = 0.5;
     bigsight.scaleY = 0.5;
     this.addChild(bigsight);
+
+    this.addEventListener('enterframe', () => {
+      let flowerSpeed = -1.2;
+      let tengokuSpeed = -0.7;
+      let bigsightSpeed = -0.5;
+
+      if (enchant.Core.instance.input.left) {
+        bigsight.x -= bigsightSpeed;
+        tengoku.x -= tengokuSpeed;
+        map.x -= flowerSpeed;
+      } else if (enchant.Core.instance.input.right) {
+        bigsight.x += bigsightSpeed;
+        tengoku.x += tengokuSpeed;
+        map.x += flowerSpeed;
+      }
+    });
   }
 }
