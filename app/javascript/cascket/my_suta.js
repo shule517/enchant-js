c5ac7cb2802e561483732cfca7e0777f4265307c
シュール <shule517@gmail.com>
2017-09-17 11:38:39 +0900

    クリックしたらすたちゅーさんが歩いていくようになった

--- app/javascript/cascket/my_suta.js
+++ app/javascript/cascket/my_suta.js
@@ -5,9 +5,13 @@ export default class MySuta extends Anime {
   constructor() {
     super();
 
-    let suta = new SutachooAnime();
-    suta.scaleX = 0.6;
-    suta.scaleY = 0.6;
-    this.addChild(suta);
+    this.suta = new SutachooAnime();
+    this.suta.scaleX = 0.6;
+    this.suta.scaleY = 0.6;
+    this.addChild(this.suta);
+  }
+
+  walking() {
+    this.suta.walking();
   }
 }
--- app/javascript/cascket/scene.js
+++ app/javascript/cascket/scene.js
@@ -9,13 +9,32 @@ export default class CascketScene extends enchant.Scene {
     let map = new Map();
     this.addChild(map);
 
-    map.addEventListener('touchstart', (e) => {
-      console.log(e.localX + " , " + e.localY);
-    });
+    // map.addEventListener('touchstart', (e) => {
+    //   console.log(e.localX + " , " + e.localY);
+    // });
 
-    // let suta = new MySuta();
-    // suta.x = 400;
-    // suta.y = 400;
-    // this.addChild(suta);
+    let suta = new MySuta();
+    suta.x = 400;
+    suta.y = 400;
+    this.addChild(suta);
+
+    map.addEventListener('touchstart', (e) => {
+      console.log(e.localX + "," + e.localY);
+
+      let diffX = map.x - (400 - e.localX);
+      let diffY = (map.y - (540 - e.localY)) * 1.5;
+      let diff = Math.sqrt(diffX * diffX + diffY * diffY);
+
+      let time = Math.abs(diffX)/2;
+      for (let i = 0; i < (time/43); i++) {
+        suta.walking();
+      }
+
+      map.tl
+        .clear()
+        .tween({x: 400 - e.localX, time: diff/3})
+        .and()
+        .tween({y: 540 - e.localY, time: diff/3});
+    });
   }
 }
