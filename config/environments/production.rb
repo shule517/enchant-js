56f081b3666531cd97fc0e347ad32c96463f4910
シュール <shule517@gmail.com>
2017-09-17 14:30:19 +0900

    Websocketの接続を許可

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # WebSocketの接続元の制限を緩める
+  config.action_cable.allowed_request_origins = [ /https?:\/\/.*/ ]
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
